a569b05383ff53142905adbb7f494b173c2e0280
bozo Božnar <gregor.boznar@gmail.com>
2022-09-25 23:15:15 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const bodyParser = require("body-parser")
+const express = require("express")
+const app = express()
+
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.listen(3000,function(){
+    console.log("listening on port 3000")
+})
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+
+  })
+
+
+  app.post("/quotes",(req,res)=>{
+    console.log(req.body)
+  })
+
+
+  
